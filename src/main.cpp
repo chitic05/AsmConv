c94eec55b01983d60787b16004453d75ae00c8de
chitic05 <chiticdavid05@gmail.com>
2025-12-18 21:43:11 +0200

    sa adaug write in memorie

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,26 @@
 #include <string>
 #include <unordered_map>
 
+#ifndef MEMSIZE
+    #define MEMSIZE 1048576 //1024*1024 = 1MB
+#endif
+
+
+
 namespace fs = std::filesystem;
 
+uint32_t generateMask(uint8_t size){
+        uint32_t mask = 0b0;
+        for(uint8_t i = 0; i < size; i++){
+            mask = (mask << 1) | 1;
+        }
+        return mask;
+    }
+
+uint8_t memory[1024*1024];
+
 namespace Registers{
-    int32_t eax, ebx, ecx, edx, esi, edi, esp, ebp;
+    int32_t eax=0x00000110, ebx, ecx, edx, esi, edi, esp=MEMSIZE, ebp;
 
     enum Reg{
         EAX, AX, AH, AL,
@@ -58,16 +74,7 @@ namespace Registers{
         [ESP] = {&esp, 32, 0},
         [EBP] = {&ebp, 32, 0}
     };
-
-    uint32_t generateMask(uint8_t size, uint8_t offset){
-        uint32_t mask = 0b0;
-        for(uint8_t i = 0; i < size; i++){
-            mask = (mask << 1) | 1;
-        }
-        mask = mask << offset;
-        return mask;
-    }
-
+    // Use to transform from text to the tag that we want
     std::unordered_map<std::string, Reg> stringToTag = {
         {"%eax", EAX}, {"%ax", AX}, {"%ah", AH}, {"%al", AL},
         {"%ebx", EBX}, {"%bx", BX}, {"%bh", BH}, {"%bl", BL},
@@ -77,64 +84,122 @@ namespace Registers{
         {"%edi", EDI},
         {"%esp", ESP},
         {"%ebp", EBP}
-    };
-
-    int32_t getRegValue(std::string regName){
-        Reg regTag;
-        if(stringToTag.find(regName) != stringToTag.end())
-            regTag = stringToTag[regName];
-        else{
-            throw std::runtime_error("Register " + regName + " doesn't exist!");
-        }            
-        RegDef registerData = regData[regTag];
-        uint32_t mask = generateMask(registerData.size, registerData.offset);
-        return *registerData.base_register & mask;
-    }
+    }; 
 }
 
+namespace Operands{
+    enum class OperandType{
+        REGISTER,
+        IMMEDIATE,
+        ADDRESS
+    };
+
+    struct Operand{
+        OperandType type;
+
+        uint8_t size;
+
+        int32_t imm;
+        uint32_t address; // for data labels(accessing memory)
+        Registers::Reg regTag; 
+          
+    };
+
+    int32_t readOperand(const Operand& op){
+        switch(op.type){
+            case OperandType::REGISTER:{
+                Registers::RegDef registerData = Registers::regData[op.regTag];
+                uint32_t mask = generateMask(registerData.size);
+                return (*registerData.base_register>>registerData.offset) & mask;
+                break;
+            }
+            case OperandType::ADDRESS:{
+                uint32_t value = 0;
+                for(uint8_t i = 0; i<op.size;i++){
+                    value |= (uint32_t)memory[op.address + i] << (8 * (op.size - 1 - i));
+                }
+
+                if(op.size == 1) return (int32_t)(int8_t)value;
+                else if(op.size == 2) return (int32_t)(int16_t)value; // now works for negative numbers
+                return (int32_t) value;
+                break;
+            }
+
+            case OperandType::IMMEDIATE:{
+                return op.imm;
+                break;
+            }
+            default:{
+                throw std::runtime_error("Ceva eroare la readOperand");
+                break;
+            }
+        }
+        return 0;
+
+    }
+    //SA FAC WRITE IN MEMORIE
+    void writeOperand(const Operand& op, int32_t value){
+        switch(op.type){
+            case OperandType::REGISTER:{
+                Registers::RegDef registerData = Registers::regData[op.regTag];
+                *registerData.base_register = value;
+                break;
+            }
+            case OperandType::ADDRESS:{
+                break;
+            }
+        }
+    }
+    
+}
 int main(int argc, char* argv[]){
 
-    if(!fs::exists("asmOut")) {
-        fs::create_directory("asmOut");
-    }
-    if(argc > 1){
-        for(size_t i = 1; i<argc; i++){
-            std::string inputFile = "./asmFiles/";
-            inputFile = inputFile + argv[i];
-            std::ifstream in(inputFile);
-            if(!in){
-                std::cerr << "File " << argv[i] << " doesn't exist";
-                continue;
-            }
+    // if(!fs::exists("asmOut")) {
+    //     fs::create_directory("asmOut");
+    // }
+    // if(argc > 1){
+    //     for(size_t i = 1; i<argc; i++){
+    //         std::string inputFile = "./asmFiles/";
+    //         inputFile = inputFile + argv[i];
+    //         std::ifstream in(inputFile);
+    //         if(!in){
+    //             std::cerr << "File " << argv[i] << " doesn't exist";
+    //             continue;
+    //         }
 
-            std::string outputFile = "./asmOut/";
-            outputFile = outputFile + argv[i];
-            std::ofstream out(outputFile);
-            if(!out){
-                std::cerr << "Problems creating the output file( " << argv[i] << " )";
-                continue;
-            }
+    //         std::string outputFile = "./asmOut/";
+    //         outputFile = outputFile + argv[i];
+    //         std::ofstream out(outputFile);
+    //         if(!out){
+    //             std::cerr << "Problems creating the output file( " << argv[i] << " )";
+    //             continue;
+    //         }
 
-            std::string line;
-            while(std::getline(in, line)){
-                std::istringstream lineWords(line); // face un input string stream din linie 
-                std::string word;
-                while(lineWords >> word){
-                    if(word == "mov" || word == "int"){
-                        break;
-                    }else out << word << " ";
-                }
-                out << '\n';
-            }
+    //         std::string line;
+    //         while(std::getline(in, line)){
+    //             std::istringstream lineWords(line); // face un input string stream din linie 
+    //             std::string word;
+    //             while(lineWords >> word){
+    //                 if(word == "mov" || word == "int"){
+    //                     break;
+    //                 }else out << word << " ";
+    //             }
+    //             out << '\n';
+    //         }
 
 
-            in.close();
-            out.close();
-        }        
-    }
-    else{
-        std::cout << "PLEASE GIVE ME AT LEAST 1 FILE!";
-    }
+    //         in.close();
+    //         out.close();
+    //     }        
+    // }
+    // else{
+    //     std::cout << "PLEASE GIVE ME AT LEAST 1 FILE!";
+    // }
 
+    Operands::Operand eaxx = {
+        .type=Operands::OperandType::REGISTER,
+        .regTag = Registers::Reg::AX
+    };
+    std::cout << std::bitset<32>(Operands::readOperand(eaxx)) << std::endl;
     return 0;
 }
